ee9e00f45c97d75ba1aa9666b446ce315f184f1d
anu-bhav <myster.anubhav@gmail.com>
2020-09-24 01:27:40 +0400

    crawler updated

--- crawl.py
+++ crawl.py
@@ -2,6 +2,8 @@ import pyautogui, os, requests, re
 from msedge.selenium_tools import Edge, EdgeOptions
 from time import sleep
 from bs4 import BeautifulSoup
+from pprint import pprint
+from collections import OrderedDict
 
 
 def Download(url):
@@ -33,8 +35,24 @@ def Crawl(url, pattern):
     soup = BeautifulSoup(req.text, "html.parser")
     print(soup.title)
 
-    for link in soup.find_all("a", {"href": re.compile(pattern)}):
-        print(link.get("href"))
+    # gets all links directly
+    # for link in soup.find_all("a", {"href": re.compile(pattern)}):
+    #     print(link.get("href"))
+
+    spans = soup.find_all("ul", {"class": "bellows-nav"})
+    unit_list = OrderedDict()
+    title_list = []
+
+    for span in spans:
+        for title in span.find_all("span", {"class": "bellows-target-title"}):
+            title_list.append(title.text)
+
+        for item2 in span.find_all("a", {"class": "bellows-target"}, href=True):
+            unit_list[item2.text] = item2["href"]
+
+    pprint(title_list)
+    pprint(unit_list)
 
 
-Crawl("https://networklessons.com/cisco/ccna-200-301", "ccna-200-301/")
+# Crawl("https://networklessons.com/cisco/ccna-200-301", "ccna-200-301/")
+Crawl("https://networklessons.com/cisco/ccna-200-301")
