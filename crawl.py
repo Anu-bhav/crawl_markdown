3ad1d0c82104ccbc6480d4a9fbd1e83ba220f286
anu-bhav <myster.anubhav@gmail.com>
2020-09-24 04:58:38 +0400

    almost completed

--- crawl.py
+++ crawl.py
@@ -6,52 +6,83 @@ from pprint import pprint
 from collections import OrderedDict
 
 
-def Download(url):
+def Download(path, url):
     # add edgedriver_win46/msedgedriver.exe to path
     options = EdgeOptions()
     options.use_chromium = True
     unpacked_extension_path = os.path.join(os.getcwd(), "markdown-clipper")
     options.add_argument("--load-extension={}".format(unpacked_extension_path))
-    download_path = os.path.join(os.getcwd(), "Markdown Output")
+    download_path = os.path.join(os.getcwd(), "Markdown Output\\", path)
     prefs = {"download.default_directory": download_path}
     options.add_experimental_option("prefs", prefs)
     driver = Edge(options=options)
 
     driver.get(url)
-    sleep(3)
+    sleep(5)
     extn = [714, 72]
-    print(extn)
+    # print(extn)
     pyautogui.click(x=extn[0], y=extn[1], clicks=1, interval=0.0, button="left")
-    sleep(1)
+    sleep(2)
     extn = pyautogui.position()
     pyautogui.click(x=extn[0], y=extn[1], clicks=1, interval=0.0, button="left")
 
 
-# Download("https://www.w3schools.com/python/python_functions.asp")
+def make_dir(path):
+    download_path = os.path.join(os.getcwd(), "Markdown Output\\", path)
+    os.makedirs(download_path, exist_ok=True)
 
 
-def Crawl(url, pattern):
+# def Crawl(url, pattern):
+def Crawl(url):
     req = requests.get(url)
     soup = BeautifulSoup(req.text, "html.parser")
-    print(soup.title)
+    course = soup.title.text.split("|")[0].strip()
 
     # gets all links directly
     # for link in soup.find_all("a", {"href": re.compile(pattern)}):
     #     print(link.get("href"))
 
     spans = soup.find_all("ul", {"class": "bellows-nav"})
-    unit_list = OrderedDict()
+
     title_list = []
+    links_list = OrderedDict()
 
     for span in spans:
         for title in span.find_all("span", {"class": "bellows-target-title"}):
-            title_list.append(title.text)
+            text = title.text.replace(":", " -")
+            title_list.append(text)
 
         for item2 in span.find_all("a", {"class": "bellows-target"}, href=True):
-            unit_list[item2.text] = item2["href"]
+            links_list[item2.text] = item2["href"]
 
-    pprint(title_list)
-    pprint(unit_list)
+    unit_index = []
+    links_index = []
+
+    for i, item in enumerate(title_list):
+        if "Unit " in item:
+            unit_index.append(i)
+
+    for link in links_list:
+        links_index.append([index for index, num in enumerate(title_list) if link in num][0])
+
+    # Make Output Directories
+    # for i in unit_index:
+    #     path = course + "\\" + title_list[i]
+    #     print(path)
+
+    for i, unit in enumerate(unit_index):
+        for link in links_index:
+            try:
+                # print(link, unit, unit_index[i + 1])
+                if (unit < link) and (link < unit_index[i + 1]):
+                    print(title_list[unit], " ---- ", title_list[link], " ---- ", links_list[title_list[link]])
+                    # Unit 1 - Introduction  ----  How to study Networking  ----  https://...
+                    path_to_download = course + "\\" + title_list[unit]
+                    link_to_download = links_list[title_list[link]]
+                    make_dir(path_to_download)
+                    Download(path_to_download, link_to_download)
+            except (IndexError, ValueError):
+                break
 
 
 # Crawl("https://networklessons.com/cisco/ccna-200-301", "ccna-200-301/")
